d54c6606365e9c21a3bd2b651dbd92cc0d128081
fieldOfView <aldo@fieldofview.com>
2025-01-27 15:37:28 +0100

    Remove balls legacy

--- __main__.py
+++ __main__.py
@@ -5,31 +5,6 @@ from hackerspaces import HackerSpacesNL
 from gpio import FirmataGPIO
 
 
-class Ball:
-    def __init__(self, screen_width, screen_height):
-        self.screen_width = screen_width
-        self.screen_height = screen_height
-        self.radius = random.randint(10, 30)
-        self.x = random.randint(self.radius, self.screen_width - self.radius)
-        self.y = random.randint(self.radius, self.screen_height - self.radius)
-
-        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        self.speed_x = random.uniform(-3, 3)
-        self.speed_y = random.uniform(-3, 3)
-
-    def update(self):
-        self.x += self.speed_x
-        self.y += self.speed_y
-
-        if self.x - self.radius < 0 or self.x + self.radius > self.screen_width:
-            self.speed_x *= -1
-        if self.y - self.radius < 0 or self.y + self.radius > self.screen_height:
-            self.speed_y *= -1
-
-    def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.radius)
-
-
 class App:
     def __init__(self):
         pygame.init()
@@ -39,7 +14,6 @@ class App:
         pygame.display.set_caption("HotelSwitch")
 
         self.clock = pygame.time.Clock()
-        self.balls = [Ball(self.screen_width, self.screen_height) for _ in range(10)]
 
         self.hsnl = HackerSpacesNL()
         self.gpio = FirmataGPIO()
@@ -60,13 +34,8 @@ class App:
 
         self.hsnl.update()
 
-        #for ball in self.balls:
-        #    ball.update()
-
     def draw(self):
         self.screen.fill((0, 0, 0))
-        #for ball in self.balls:
-        #    ball.draw(self.screen)
 
         if self.hsnl.lat_range.range is not None and self.hsnl.lon_range.range is not None:
             for space in self.hsnl.spaces:
